f3a2af83e405621c35ff39afdcadc5b35abf9dfb
MaxLKP <maximilian.lkp@gmail.com>
2026-01-30 22:54:53 +0100

    Added config for database setting and readout

--- sensor_readout.py
+++ sensor_readout.py
@@ -1,20 +1,34 @@
 from influxdb import InfluxDBClient
 from sensors import *
 import time
+import yaml
+
+config_file = "/home/blueberry/sensors/config.yaml"
+
+with open (config_file , 'r') as file:
+    config = yaml.safe_load(file)
+
+db = config["database"]["name"]
+frequency = int(config["readout"]["delay"])
+output = config["readout"]["output"]
 
 bme280 = bme280(0x76, 1)
 
-#client = InfluxDBClient(host = 'localhost', port = 8086, dbname = 'db')
 client = InfluxDBClient(host = 'localhost', port = 8086)
-client.switch_database('db')
+client.switch_database(db)
 
-while True:
-    temp = round(bme280.get_temp(), 2)
-    press = round(bme280.get_press(), 2)
-    hum = round(bme280.get_hum(), 2)
-    print(f"Wrote to DB: {temp}, {press}, {hum}")
-    client.write_points([{"measurement": "bme280_temp", "tags": {"location": "blueberry"}, "fields": {"value": temp}}])
-    client.write_points([{"measurement": "bme280_press", "tags": {"location": "blueberry"}, "fields": {"value": press}}])
-    client.write_points([{"measurement": "bme280_hum", "tags": {"location": "blueberry"}, "fields": {"value": hum}}])
-    time.sleep(5)
-client.close()
+try:
+    while True:
+        temp = round(bme280.get_temp(), 2)
+        press = round(bme280.get_press(), 2)
+        hum = round(bme280.get_hum(), 2)
+        if output == True:
+            print(f"Wrote to DB: {temp}, {press}, {hum}")
+        else: pass
+        client.write_points([{"measurement": "bme280_temp", "tags": {"location": "blueberry"}, "fields": {"value": temp}}])
+        client.write_points([{"measurement": "bme280_press", "tags": {"location": "blueberry"}, "fields": {"value": press}}])
+        client.write_points([{"measurement": "bme280_hum", "tags": {"location": "blueberry"}, "fields": {"value": hum}}])
+        time.sleep(frequency)
+except KeyboardInterrupt:
+    client.close()
+    print("Terminated Sensor Readout. Client closed.")
