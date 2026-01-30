c9b736282cfba243e535f8a44d6410235be6a102
MaxLKP <maximilian.lkp@gmail.com>
2026-01-30 22:36:48 +0100

    Added Influx db data writing

--- sensor_readout.py
+++ sensor_readout.py
@@ -1,5 +1,6 @@
 from influxdb import InfluxDBClient
 from sensors import *
+import time
 
 bme280 = bme280(0x76, 1)
 
@@ -7,11 +8,13 @@ bme280 = bme280(0x76, 1)
 client = InfluxDBClient(host = 'localhost', port = 8086)
 client.switch_database('db')
 
-temp = bme280.get_temp()
-press = bme280.get_press()
-hum = bme280.get_hum()
-
-client.write_points([{"measurement": "bme280", "tags": {"location": "blueberry"}, "fields": {"value": temp}}])
-client.write_points([{"measurement": "bme280", "tags": {"location": "blueberry"}, "fields": {"value": press}}])
-client.write_points([{"measurement": "bme280", "tags": {"location": "blueberry"}, "fields": {"value": hum}}])
+while True:
+    temp = round(bme280.get_temp(), 2)
+    press = round(bme280.get_press(), 2)
+    hum = round(bme280.get_hum(), 2)
+    print(f"Wrote to DB: {temp}, {press}, {hum}")
+    client.write_points([{"measurement": "bme280_temp", "tags": {"location": "blueberry"}, "fields": {"value": temp}}])
+    client.write_points([{"measurement": "bme280_press", "tags": {"location": "blueberry"}, "fields": {"value": press}}])
+    client.write_points([{"measurement": "bme280_hum", "tags": {"location": "blueberry"}, "fields": {"value": hum}}])
+    time.sleep(5)
 client.close()
